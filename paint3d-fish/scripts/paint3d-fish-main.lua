4b43a636187d76bb8a68d934203ce28d44902e91
oO <47592289+oocytanb@users.noreply.github.com>
2019-06-23 15:11:58 +0900

    Change: paint3d-fish のスクリプトを vci.message の確認用に

--- paint3d-fish/scripts/paint3d-fish-main.lua
+++ paint3d-fish/scripts/paint3d-fish-main.lua
@@ -3,12 +3,31 @@
 --  MIT Licensed
 ----------------------------------------------------------------
 
--- SubItem をトリガーでつかむと呼び出される。
-function onGrab(target)
-    print("onGrab: " .. target)
-end
+-- vci.message の送受信を確認するためのスクリプト。
 
 -- グリップしてアイテムを使用すると呼び出される。
 function onUse(use)
-    print("onUse: " .. use)
+    print('onUse: ' .. use)
+
+    vci.message.Emit('print-msg', 'fish')
+
+    -- value に nil を指定すると、nil として受信される。
+    vci.message.Emit('print-msg-nil', nil)
+
+    -- value を省略すると、そのメッセージは受信されない。
+    vci.message.Emit('print-msg-no-value-arg')
 end
+
+local function printMessage(sender, name, message)
+    local buf = '\non ' .. name .. '\n'
+    for k, v in pairs(sender) do
+        buf = buf .. '  sender.'  .. tostring(k) .. ' : ' .. tostring(v) .. '\n'
+    end
+    buf = buf .. '  message : ' .. tostring(message) .. '\n'
+    print(buf)
+end
+
+vci.message.On('print-msg', printMessage)
+vci.message.On('print-msg-nil', printMessage)
+vci.message.On('print-msg-no-value-arg', printMessage)
+vci.message.On('comment', printMessage)
