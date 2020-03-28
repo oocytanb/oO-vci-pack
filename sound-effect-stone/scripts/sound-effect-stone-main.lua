741d1c833e362fb961d4dfcfa1b33053d298732c
oO <47592289+oocytanb@users.noreply.github.com>
2020-03-28 09:16:30 +0900

    Change object names of stones

--- sound-effect-stone/scripts/sound-effect-stone-main.lua
+++ sound-effect-stone/scripts/sound-effect-stone-main.lua
@@ -1,16 +1,16 @@
 -- SPDX-License-Identifier: MIT
 -- Copyright (c) 2019 oO (https://github.com/oocytanb)
 
-local locus_efk = vci.assets.GetEffekseerEmitter("sound-effect-stone-locus")
-local ripple_efk = vci.assets.GetEffekseerEmitter("sound-effect-stone-ripple")
+local locus_efk = vci.assets.GetEffekseerEmitter("green-stone")
+local ripple_efk = vci.assets.GetEffekseerEmitter("blue-stone")
 
 onUse = function (use)
-    if use == "sound-effect-stone-locus" then
-        -- "sound-effect-stone-locus" のアイテムがグリップされたら、音声とエフェクトを再生する
+    if use == "green-stone" then
+        -- "green-stone" のアイテムがグリップされたら、音声とエフェクトを再生する
         vci.assets.audio._ALL_Play("sound-locus-se", 1, false)
         locus_efk._ALL_Play()
-    elseif use == "sound-effect-stone-ripple" then
-        -- "sound-effect-stone-ripple" のアイテムがグリップされたら、音声とエフェクトを再生する
+    elseif use == "blue-stone" then
+        -- "blue-stone" のアイテムがグリップされたら、音声とエフェクトを再生する
         vci.assets.audio._ALL_Play("sound-ripple-se", 1, false)
         ripple_efk._ALL_PlayOneShot()
     end
