fbfeaaf27ee337716652812e73b3d537cd81c5b4
oO <47592289+oocytanb@users.noreply.github.com>
2021-02-15 20:22:23 +0900

    Rename animations

--- cykhu_land/scripts/cykhu_land_main.lua
+++ cykhu_land/scripts/cykhu_land_main.lua
@@ -1,4 +1,4 @@
 -- SPDX-License-Identifier: MIT
 -- Copyright (c) 2021 oO (https://github.com/oocytanb)
 
-vci.assets.PlayAnimationFromName("cykhu_land_sky_rotation", true)
+vci.assets.PlayAnimationFromName("cykhu_land_sky_anim", true)
